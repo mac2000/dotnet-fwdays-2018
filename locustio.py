d8cd195430ab139cb791e985a8cec38efba8bf73
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2018-03-31 14:20:42 +0300

    cloud initial

--- locustio.py
+++ locustio.py
@@ -7,9 +7,11 @@ def load(l):
     l.client.get("/api/demo/load")
 
 class UserBehavior(TaskSet):
-    tasks = {ping: 20, load: 1}
+    tasks = {ping: 20, load: 10}
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
     min_wait = 5000
     max_wait = 9000
+
+# locust --host=http://35.195.11.75 -f locustio.py
